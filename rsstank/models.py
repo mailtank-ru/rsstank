d9b29e667f093e92f5d9c2005ca3455c0cb20ef5
Anton Romanovich <anthony.romanovich@gmail.com>
2013-11-20 12:33:12 +0600

    Fix and document models

--- rsstank/models.py
+++ rsstank/models.py
@@ -1,50 +1,59 @@
+# coding: utf-8
 import datetime
 
 from . import db
 
 
-class MailtankAccessKey(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class AccessKey(db.Model):
+    """Ключ доступа к API Mailtank."""
 
-    content = db.Column(db.String, nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    
+    #: Содержимое ключа доступа
+    content = db.Column(db.String(255), nullable=False, unique=True)
+    #: Включен ли функционал rsstank для данного ключа?
     is_enabled = db.Column(db.Boolean, nullable=False, default=False)
-    namespace = db.Column(db.String, nullable=False)
+    #: Пространство имён ключа (используется для ограничения множества
+    #: тегов, с которыми работает rsstank)
+    namespace = db.Column(db.String(255), nullable=False)
 
 
 class Feed(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    """Фид."""
 
+    id = db.Column(db.Integer, primary_key=True)
     access_key_id = db.Column(db.Integer, db.ForeignKey('access_key.id'),
                               nullable=False)
+    #: URL RSS-фида
     url = db.Column(db.String(2000), nullable=False)
+    #: Интервал в секундах, с которым генерируются рассылки
+    #: с новыми элементами фида
     sending_interval = db.Column(db.Integer, nullable=False)
-    last_polled_at = db.Column(db.Datetime)
-    last_sent_at = db.Column(db.Datetime)
-
-
-class FeedItemEnclosure(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    feed_item_id = db.Column(db.Integer, db.ForeignKey('feed_item.id'),
-                             nullable=False)
-    length = db.Column(db.Integer, nullable=False)
-    type = db.Column(db.String(500), nullable=False)
-    url = db.Column(db.String(2000), nullable=False)
-
-
-class FeedItemSource(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    feed_item_id = db.Column(db.Integer, db.ForeignKey('feed_item.id'),
-                             nullable=False)
-    url = db.Column(db.String(2000), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+    #: Дата и время последнего обновления элементов фида
+    last_polled_at = db.Column(db.DateTime)
+    #: Дата и время последнего создания рассылки по фиду
+    last_sent_at = db.Column(db.DateTime)
+    #: Ключ доступа к Mailtank API, к которому привязан фид
+    access_key = db.relationship(
+        'AccessKey',
+        backref=db.backref('feeds', lazy='dynamic', cascade='all'))
 
 
 class FeedItem(db.Model):
+    """Элемент фида."""
+
     id = db.Column(db.Integer, primary_key=True)
-    polled_at = db.Column(db.Datetime, nullable=False,
-                          default=datetime.datetime.utcnow)
+    feed_id = db.Column(db.Integer, db.ForeignKey('feed.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.datetime.utcnow)
+    
+    feed = db.relationship(
+        'Feed', backref=db.backref('items', lazy='dynamic', cascade='all'))
+
+    # XXX TODO Нижеследующие поля отражают стандарт RSS.
+    # Вероятно, стоит сделать набор полее более общим, так, чтобы
+    # он стал подходящим для хранения элементов и Atom-фидов.
+
     # Required.  Defines the title of the item
     title = db.Column(db.String(2000), nullable=False)
     # Required. Defines the hyperlink to the item
@@ -57,11 +66,17 @@ class FeedItem(db.Model):
     category = db.Column(db.String(1000))
     # Optional. Allows an item to link to comments about that item
     comments = db.Column(db.String(2000))
-    # Optional. Allows a media file to be included with the item
-    enclosure_id = db.Column(db.Integer, db.ForeignKey('feed_item_enclosure.id'))
+
     # Optional. Defines a unique identifier for the item
     guid = db.Column(db.String(1000))
     # Optional. Defines the last-publication date for the item
-    pub_date = db.Column(db.Datetime)
+    pub_date = db.Column(db.DateTime)
+
+    # Optional. Allows a media file to be included with the item
+    enclosure_length = db.Column(db.Integer)
+    enclosure_type = db.Column(db.String(500))
+    enclosure_url = db.Column(db.String(2000))
+
     # Optional. Specifies a third-party source for the item
-    source_id = db.Column(db.Integer, db.ForeignKey('feed_item_source.id'))
+    source_url = db.Column(db.String(2000))
+    source_content = db.Column(db.String(2000))
