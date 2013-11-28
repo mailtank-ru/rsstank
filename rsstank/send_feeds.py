e1f1f5ea3037aafd1f41a18db20d42b2a600d943
Max Pomazuev <max.pomazuev@gmail.com>
2013-11-28 18:20:56 +0600

    Introducing check for uniqueness of feed items during send_feeds command

--- rsstank/send_feeds.py
+++ rsstank/send_feeds.py
@@ -12,10 +12,20 @@ logger = logging.getLogger(__name__)
 def send_feed(feed):
     """Создаёт рассылку, содержащую новые элементы из фида `feed`."""
     items_to_send = feed.items.order_by(FeedItem.pub_date)
+
     if feed.last_sent_at:
         items_to_send = items_to_send.filter(
             FeedItem.created_at >= feed.last_sent_at)
-    context_items = [item.to_context_entry() for item in items_to_send]
+
+    items_dict = {}
+    for item in items_to_send:
+        existed_item = items_dict.get(item.guid, None)
+        if not existed_item or item.pub_date > existed_item.pub_date:
+            # Элемент фида с таким guid встречается первый раз, либо он более
+            # свежий, чем уже встречавшийся
+            items_dict[item.guid] = item
+
+    context_items = [item.to_context_entry() for item in items_dict.values()]
     assert context_items  # Потому что никто (никто!) не смеет посылать
                           # пустую рассылку
     try:
