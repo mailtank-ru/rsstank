844fa591ae1cef57f73d591014b1919d6a3d5edd
Anton Romanovich <anthony.romanovich@gmail.com>
2013-12-06 16:33:21 +0600

    Log manage.py errors to Sentry

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+import functools
+
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -8,6 +10,24 @@ from rsstank.poll_feeds import main as poll_feeds
 from rsstank.send_feeds import main as send_feeds
 from rsstank.update_feeds import main as update_feeds
 from rsstank.cleanup import main as cleanup
+
+
+def sentrify(f):
+    """Декоратор, логирующий все исключения функции в Sentry (
+    :obj:`rsstank.sentry`.
+    """
+    if not rsstank.sentry:
+        return f
+
+    @functools.wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except:
+            rsstank.sentry.captureException()
+            raise
+
+    return decorated_function
 
 
 # Присваиваем функциям осмысленные имена (изначально они все
@@ -21,10 +41,9 @@ cleanup.__name__ = 'cleanup'
 
 manager = Manager(rsstank.app)
 manager.add_command('db', MigrateCommand)
-manager.command(poll_feeds)
-manager.command(send_feeds)
-manager.command(update_feeds)
-manager.command(cleanup)
+
+for command in (poll_feeds, send_feeds, update_feeds, cleanup):
+    manager.command(sentrify(command))
 
 
 if __name__ == '__main__':
