03f5d46cba92d4bc34a1100a519bbe52f0a95ecc
Anton Romanovich <anthony.romanovich@gmail.com>
2013-12-08 21:56:06 +0600

    Debug kozmic.sh

--- kozmic.sh
+++ kozmic.sh
@@ -2,6 +2,7 @@
 /usr/bin/mysqld_safe &
 mysql -e 'create database rsstank_test character set utf8 collate utf8_general_ci;'
 
+pip install distribute==0.6.34
 pip install -r ./requirements/basic.txt
 pip install -r ./requirements/dev.txt
 
