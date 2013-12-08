2ad52827258b17407d3d383b53dcc3dcc6d51034
Anton Romanovich <anthony.romanovich@gmail.com>
2013-12-09 01:31:15 +0600

    Bump Kozmic CI

--- kozmic.sh
+++ kozmic.sh
@@ -2,7 +2,6 @@
 /usr/bin/mysqld_safe &
 mysql -e 'create database rsstank_test character set utf8 collate utf8_general_ci;'
 
-easy_install distribute==0.6.34
 pip install -r ./requirements/basic.txt
 pip install -r ./requirements/dev.txt
 
