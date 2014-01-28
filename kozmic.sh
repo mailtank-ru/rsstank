8be3802c27854605a5c608ab88afdf333f7c1346
Anton Romanovich <anthony.romanovich@gmail.com>
2014-01-28 14:07:10 +0600

    Fix kozmic.sh

--- kozmic.sh
+++ kozmic.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -e
+
+sudo su <<EOF
 /usr/bin/mysqld_safe &
 sleep 5
 mysql -e 'create database rsstank_test character set utf8 collate utf8_general_ci;'
 
 pip install -r ./requirements/basic.txt
 pip install -r ./requirements/dev.txt
+EOF
+
 cp ./rsstank/config_local.py-travis ./rsstank/config_local.py
 ./test.sh
